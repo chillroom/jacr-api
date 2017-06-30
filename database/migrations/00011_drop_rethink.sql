a2928df169d8c4c44352eb20e75b73e315e77ef1
Qais Patankar <qaisjp@gmail.com>
2017-06-30 14:41:52 +0100

    tweak rethinkid drop migration

--- database/migrations/00011_drop_rethink.sql
+++ database/migrations/00011_drop_rethink.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-DROP INDEX public.dubtrack_users_rethinkid_uindex RESTRICT;
-ALTER TABLE public.dubtrack_users DROP rethink_id;
+DROP INDEX if exists public.dubtrack_users_rethinkid_uindex RESTRICT;
+ALTER TABLE public.dubtrack_users DROP if exists rethink_id;
 
-DROP INDEX public.songs_rethink_id_uindex RESTRICT;
-ALTER TABLE public.songs DROP rethink_id;
+DROP INDEX if exists public.songs_rethink_id_uindex RESTRICT;
+ALTER TABLE public.songs DROP if exists rethink_id;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
